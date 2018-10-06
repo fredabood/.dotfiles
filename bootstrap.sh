6fa2824282c734107e1a4867957876fc74a83a98
Fred Abood <fred@fredabood.com>
2018-10-06 13:38:23 -0500

    fixing bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,6 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-# Personal Git Config
-git config --global user.name "Fred Abood";
-git config --global user.email fred@fredabood.com;
-
 bash packages.sh
 
 # Taken from https://github.com/CodyReichert/dotfiles/blob/master/install.sh
@@ -14,6 +10,12 @@ for d in `ls .`;
 do
   ( stow $d )
 done
+
+source ~/.bash_profile
+
+# Personal Git Config
+git config --global user.name "Fred Abood";
+git config --global user.email fred@fredabood.com;
 
 bash conda.sh
 bash ssh.sh
--- conda.sh
+++ conda.sh
@@ -5,9 +5,9 @@ elif [ "${KERNEL:0:5}" = "Linux" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
 fi
 
-bash $HOME/miniconda.sh -b -p $HOME/.conda && \
-echo export PATH="$HOME/.conda/bin:$PATH" >> $HOME/.extra && \
-rm $HOME/miniconda.sh && \
+bash $HOME/miniconda.sh -b -p $HOME/.conda
+echo export PATH="$HOME/.conda/bin:$PATH" >> $HOME/.extra
+rm $HOME/miniconda.sh
 source $HOME/.bash_profile
 
 pip install --upgrade pip && conda update conda
