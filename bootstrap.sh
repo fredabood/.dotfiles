7b10e6532e0ba6892ce617e01ebca3e9b067c74f
Fred Abood <fred@fredabood.com>
2018-10-05 20:42:13 -0500

    init commit, forking Mathias' dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
-mkdir $HOME/.dotfiles
-
-cd "$(dirname "${BASH_SOURCE}")";
+# mkdir $HOME/.dotfiles
+#
+# cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "atom.sh" \
-		--exclude "conda.sh" \
-		--exclude "remote.sh" \
-		--exclude "brew.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . $HOME/.dotfiles;
-	source ~/.dotfiles/.bash_profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+# function doIt() {
+# 	rsync --exclude ".git/" \
+# 		--exclude ".DS_Store" \
+# 		--exclude ".osx" \
+# 		--exclude "bootstrap.sh" \
+# 		--exclude "atom.sh" \
+# 		--exclude "conda.sh" \
+# 		--exclude "remote.sh" \
+# 		--exclude "brew.sh" \
+# 		--exclude "README.md" \
+# 		--exclude "LICENSE-MIT.txt" \
+# 		-avh --no-perms . $HOME/.dotfiles;
+# 	source ~/.dotfiles/.bash_profile;
+# }
+#
+# if [ "$1" == "--force" -o "$1" == "-f" ]; then
+# 	doIt;
+# else
+# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+# 	echo "";
+# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+# 		doIt;
+# 	fi;
+# fi;
+# unset doIt;
 
 KERNEL=$(uname -a)
 if [ "${KERNEL:0:6}" = "Darwin" ]; then
@@ -43,8 +43,12 @@ elif [ "${KERNEL:0:5}" = "Linux" ]; then
 	sudo apt-get install -y stow && \
 	sudo apt-get install -y r-base && \
 	sudo apt-get install -y nodejs && \
+	sudo apt install python-pip && \
 	bash conda.sh && \
 	bash remote.sh;
 fi
 
-cd $HOME && stow $HOME/.dotfiles;
+for folder in ./
+	do [[ -f $folder ]] && stow -R $folder
+  do [[ -d $folder ]] && stow -R $folder
+done
