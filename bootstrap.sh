68287114cc27eb8cec7bcb5332bc9815707669ef
Fred Abood <fred@fredabood.com>
2018-10-06 21:38:31 -0500

    fixing bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,4 +34,23 @@ bash conda.sh
 git config --global user.name "Fred Abood";
 git config --global user.email fred@fredabood.com;
 
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+  ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C "fred@fredabood.com";
+  echo "
+  Host github.com
+    HostName github.com
+    User git
+    IdentityFile $HOME/.ssh/id_rsa
+  Host gitlab.com
+    HostName gitlab.com
+    User git
+    PubkeyAuthentication yes
+    IdentityFile $HOME/.ssh/id_rsa
+  Host nu.bootcampcontent.com
+    HostName nu.bootcampcontent.com
+    User git
+    IdentityFile $HOME/.ssh/id_rsa
+  " >> ~/.ssh/config
+fi
+
 source $HOME/.bash_profile
