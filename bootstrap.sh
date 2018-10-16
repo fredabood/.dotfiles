7b38db7271d5e4d2f38b9dcba5ceef347c7a2e30
Fred Abood <fred@fredabood.com>
2018-10-16 07:35:38 -0500

    git username/email in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,21 @@
 # In order to use the rsync function instead of stow, cd into the home directory
 cd "$(dirname "${BASH_SOURCE}")";
 
+USERNAME=$(git config --global user.name)
+if [ "$USERNAME"="" ]; then
+	read -p "What is your Git username i.e. Mona Lisa?" $USERNAME
+fi
+
+EMAIL=$(git config --global user.email)
+if [ "$EMAIL"="" ]; then
+	read -p "What is your Git email i.e. name@example.com?" $EMAIL
+fi
+
 git pull origin master;
+
+git config --global user.name "$USERNAME";
+git config --global user.email "$EMAIL";
+git config --global --unset commit.gpgsign;
 
 # Only auto-runs brew.sh if the ~/.bash_profile isn't a symlink
 if [ ! -L $HOME/.bash_profile ]; then
@@ -51,29 +65,5 @@ unset doIt;
 if [ ! -d $HOME/.conda ]; then
 	bash conda.sh
 fi
-
-# Git Config
-
-# USERNAME=$(git config --global user.name)
-# if [ "$USERNAME"="" ]; then
-# 	echo What is your Git username i.e. Mona Lisa?
-# 	read FIRST LAST
-# 	git config --global user.name "$FIRST $LAST";
-# 	unset FIRST; unset LAST;
-# fi
-# unset USERNAME;
-#
-# EMAIL=$(git config --global user.email)
-# if [ "$USERNAME"="" ]; then
-# 	echo What is your Git email i.e. name@example.com?
-# 	read GIT_EMAIL
-# 	git config --global user.email $GIT_EMAIL;
-# 	unset GIT_EMAIL;
-# fi
-# unset EMAIL;
-
-git config --global user.name "Fred Abood"
-git config --global user.email fred@fredabood.com
-git config --global --unset commit.gpgsign
 
 source $HOME/.bash_profile
