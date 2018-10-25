4d02c4b982f3f7b7f49e4739b29a7fe11fb1f348
Fred Abood <fred@fredabood.com>
2018-10-24 21:30:27 -0500

    finalized dockerfile without jupyter

--- home/.jupyter/jupyter_notebook_config.py
+++ home/.jupyter/jupyter_notebook_config.py
@@ -1,9 +1,9 @@
 c = get_config()
 
 # Notebook config this is where you saved your pem cert
-c.NotebookApp.certfile = u'$HOME/.certs/jupyter_cert.pem'
+c.NotebookApp.certfile = u"$HOME/.jupyter/certs/jupyter_cert.pem"
 # Run on all IP addresses of your instance
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = "*"
 # Don't open browser by default
 c.NotebookApp.open_browser = False
 # Fix port to 8888
